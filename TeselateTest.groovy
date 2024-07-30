c0e27c5f6f78c71c8b33753914f7e459be9eceeb
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-30 17:04:50 -0400

    Special case for hexagons

--- TeselateTest.groovy
+++ TeselateTest.groovy
@@ -3,7 +3,14 @@ import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Hexagon
 
 // code here
-
+List<CSG> tesselateHex(CSG incoming,int xSteps, int ySteps, double spacing){
+		double y= incoming.getTotalY()+spacing
+		double x =(((y/Math.sqrt(3))))*3/2
+		return tesselate(incoming,xSteps,ySteps,x,y,y/2);
+	}
+List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, double oddRowYOffset){
+		return tesselate(incoming,xSteps,ySteps,incoming.getTotalX(),incoming.getTotalY(),oddRowYOffset);
+	}
 List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, double xGrid, double yGrid, double oddRowYOffset){
 		ArrayList<CSG> back = new ArrayList<CSG>();
 		for(int i=0;i<xSteps;i++) {
@@ -15,4 +22,5 @@ List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, double xGrid, double yG
 		return back;
 	}
 
-return tesselate(new Hexagon(9,10).toCSG(),5,5,8.75,10,5)
+//return tesselate(new Hexagon(9,10).toCSG(),5,5,8.75,10,5)
+return tesselateHex(new Hexagon(9,10).toCSG(),5,5,1)
