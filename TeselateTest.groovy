d8cefa11092d7a0858928bc60f41e1bb1f980bce
Jansen Smith <jansen@bstarengineering.com>
2024-07-30 18:13:46 -0400

    developed and tested z layering

--- TeselateTest.groovy
+++ TeselateTest.groovy
@@ -4,15 +4,19 @@ import eu.mihosoft.vrl.v3d.Hexagon
 
 // code here
 
-List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, double xGrid, double yGrid, double oddRowYOffset){
+List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, int zSteps, double xGrid, double yGrid, double zGrid, double oddRowXOffset, double oddColYOffset, double oddLayZOffset){
 		ArrayList<CSG> back = new ArrayList<CSG>();
 		for(int i=0;i<xSteps;i++) {
 			for(int j=0;j<ySteps;j++) {
-				double yoff = i%2==0?0:oddRowYOffset;
-				back.add(incoming.move(((double)i)*xGrid,yoff+(((double)j)*yGrid), 0));
+				for(int k=0;k<zSteps;k++) {
+					double xoff = i%2==0?0:oddRowXOffset;
+					double yoff = j%2==0?0:oddColYOffset;
+					double zoff = k%2==0?0:oddLayZOffset;
+					back.add(incoming.move(xoff+(((double)i)*xGrid),yoff+(((double)j)*yGrid), zoff+(((double)k)*zGrid)));
+				}
 			}
 		}
 		return back;
 	}
 
-return tesselate(new Hexagon(9,10).toCSG(),5,5,8.75,10,5)
+return tesselate(new Sphere(3).toCSG(),5,5,3,10,10,10,0,0,0)
