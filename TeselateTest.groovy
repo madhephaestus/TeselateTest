f289153d8863c620e2835a3434b39ba9d1899365
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-30 16:37:24 -0400

    Adding paraams

---
+++ TeselateTest.groovy
@@ -0,0 +1,18 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Hexagon
+
+// code here
+
+List<CSG> tesselate(CSG incoming,int xSteps, int ySteps, double xGrid, double yGrid, double oddRowYOffset){
+		ArrayList<CSG> back = new ArrayList<CSG>();
+		for(int i=0;i<xSteps;i++) {
+			for(int j=0;j<ySteps;j++) {
+				double yoff = i%2==0?0:oddRowYOffset;
+				back.add(incoming.move(((double)i)*xGrid,yoff+(((double)j)*yGrid), 0));
+			}
+		}
+		return back;
+	}
+
+return tesselate(new Hexagon(10,10).toCSG(),5,5,10,5)
